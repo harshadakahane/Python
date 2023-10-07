40a0ee3411cd5d6912e65f099bb3375a77899e2e
harshadakahane <harshadakahane281@gmail.com>
2023-10-07 16:04:27 +0530

    Assignment operators in python

---
+++ Operators/AssignmentOperator.py
@@ -0,0 +1,41 @@
+#Assignment operators
+a=10
+print("simple assignment opeartor")
+print(a)
+print()
+
+
+print("Addition assignment operator")
+a+=5
+print(a)
+print()
+
+print("Subtraction assignment opeartor")
+a-=9
+print(a)
+print()
+
+print("Multipliaction assigmnet opeartor")
+a*=8
+print(a)
+print()
+
+print("Divsion assignmnet opeartor")
+a/=9
+print(a)
+print()
+
+print("Modulus assignment opeartor")
+a%=9
+print(a)
+print()
+
+print("Floor division assignment opeartor")
+a//=6
+print(a)
+print()
+
+print("Exponentional assignmnet operator")
+a**=8
+print(a)
+print()
