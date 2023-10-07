06e7164424377807f1c520da58c79e8d828b5d4e
harshadakahane <harshadakahane281@gmail.com>
2023-10-07 15:51:04 +0530

    Relational operators in python

---
+++ Operators/RelationalOperator.py
@@ -0,0 +1,27 @@
+# Relational opeartor
+
+#1) Greater the >
+a=10
+b=20
+print(a>b)
+print()
+
+ #2) less then <
+print(a<b)
+print()
+
+ #3) greater the equal to >=
+print(a>=b)
+print()
+
+ #4)less then equal to <=
+print(a<=b)
+print()
+
+ #5) equal to equal to ==
+print(a==b)
+print()
+
+ #6) not equal to
+print(a!=b)
+print()
