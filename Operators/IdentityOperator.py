79082e396176f2355167f07ed8a7ea4a6115f66c
harshadakahane <harshadakahane281@gmail.com>
2023-10-07 16:11:20 +0530

    Identity operators in python

---
+++ Operators/IdentityOperator.py
@@ -0,0 +1,16 @@
+listdata =[10,20,30,40,50]
+x=10
+y=19
+print(id(x))
+print()
+print(id(y))
+print()
+
+# is identity opeartir
+print("is Identity opeartor")
+print( x is y)
+print()
+
+
+print("is not identity opeartor")
+print(x is not y)
