a0f390bce4cd7a04bda4cab5c2c208eea3861c3b
harshadakahane <harshadakahane281@gmail.com>
2023-10-07 15:46:11 +0530

    Arithmetic operators in python

---
+++ Operators/ArmithmeticOperator.py
@@ -0,0 +1,35 @@
+#Aritmetic opeartors
+
+#1) Addition operartor
+a=10
+b=20
+ans = a+b
+print(ans)
+print()
+
+# 2) Subtraction 
+
+ans = a - b
+print(ans)
+print()
+
+# 3) Mutiplication
+
+print(a * b)
+print()
+# 4) Divison
+
+print( a/b)
+print()
+
+# 5) Modulus
+print(a%b)
+print()
+
+# 6) floor division
+print(a//b)
+print()
+
+# 7) power
+print(a**b)
+print()
