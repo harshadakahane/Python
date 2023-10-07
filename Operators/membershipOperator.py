6714868127159dcc4d5f7d104448532eece11356
harshadakahane <harshadakahane281@gmail.com>
2023-10-07 16:15:14 +0530

    Membership operators in python

---
+++ Operators/membershipOperator.py
@@ -0,0 +1,16 @@
+listdata =[10,20,30,40,50]
+
+x=19
+y=20
+print("in membership operator")
+print( x in listdata)
+print()
+
+print("not in membership opeartor")
+print(x not in listdata)
+print()
+
+print(y in listdata)
+
+
+
